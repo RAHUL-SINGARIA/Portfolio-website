67d3f92c9544a710ba154e0d90dc2fde995739d1
rahul singaria <singariarahul345@gmail.com>
2023-08-23 15:52:51 +0530

    photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import myimage from '../assets/me.jpg'
+
 
 const About = () => {
   return (
@@ -13,11 +15,13 @@ const About = () => {
           <div></div>
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi. I'm Rahul Singaria, nice to meet you. Please take a look around.</p>
-            </div>
-            <div>
-              <p>I am an enthusiastic student with a deep-seated passion for the realm of software development. My unwavering focus on mastering data structures and algorithms propels me to create solutions that are not only innovative but also highly efficient. I firmly believe in the transformative potential of software to bring about positive changes in the world.</p>  
+           <div>
+            <img src={myimage} alt="img" className="rounded-2xl mx-auto w-2/3 md:w-ful" />
+           </div>
+            <div >
+              <p className='text-1xl inline  '>I am a passionate software development enthusiast, equipped with a versatile skillset that demonstrates my prowess in critical thinking, creative problem-solving, and software craftsmanship. My journey in web development has allowed me to sculpt meaningful projects that leave an impact. Proficient in the realm of data structures and algorithms, coupled with my experience in competitive programming, I possess the ability to architect efficient solutions that deliver exceptional outcomes.
+
+Driven by a strong sense of intellectual agility and a knack for rapid problem resolution, I am wholeheartedly drawn to collaborative ventures involving intriguing projects. My enthusiasm for continuous growth propels me to embrace novel challenges, which in turn catalyze my ability to conceive innovative solutions that transcend conventional boundaries.</p>  
             </div>
           </div>
       </div>
